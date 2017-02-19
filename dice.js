638731a510793fb64da62823992d79b19b1083b8
ejmears <elijah.on.board@gmail.com>
2017-02-19 18:36:17 -0500

    Total Redesign
    Totally new redesign, yay

--- dice.js
+++
@@ -1,7 +0,0 @@
-function dice()
-{
-	var x = Math.floor((Math.random()*6) + 1);
-	var y = Math.floor((Math.random()*6) + 1);
-	document.getElementById("answer1").src=x+".gif";
-	document.getElementById("answer2").src=y+".gif";
-}
